65250c53dac9fb4f8ecfede6567bf79c4c84c236
ligeralde <43016748+ligeralde@users.noreply.github.com>
2026-01-15 15:47:18 -0500

    Update image loading and processing in demo

--- faster_rcnn_demo.py
+++ faster_rcnn_demo.py
@@ -1,12 +1,15 @@
 import torch
-from torchvision.io import read_image
+from PIL import Image
+from torchvision.transforms.functional import to_tensor
 from torchvision.models.detection import fasterrcnn_resnet50_fpn, FasterRCNN_ResNet50_FPN_Weights
 
 weights = FasterRCNN_ResNet50_FPN_Weights.DEFAULT
 model = fasterrcnn_resnet50_fpn(weights=weights)
 model.eval()
 
-img = read_image("assets/street.jpg").float() / 255.0  # [C,H,W] in 0..1
+img = Image.open("assets/busy-street-with-people-and-traffic-in-accra-ghana-DXYYKT.jpg").convert("RGB")
+img = to_tensor(img)  # [C,H,W], values in [0,1]
+
 with torch.no_grad():
     preds = model([img])[0]  # dict with boxes, labels, scores
 
